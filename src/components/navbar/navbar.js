609086ae85f46231069811c7827f7291e9a8f5df
Joel Fernandes <joelandre333@gmail.com>
2021-12-09 00:16:17 -0300

    continuando estilizacao da navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,29 +3,25 @@ import "./navbar.css";
 import logo from "../../images/logo.svg";
 import {Button} from "../button/button";
 import menu from "../../icons/bars-solid.svg"
-
+import {FaBars} from 'react-icons/fa';
 
 function Navbar() {
     
 
     return (
         <nav className="nav-geral">
-            
-            <div className="contain-logo">
-                <a className="link-logo" href="/"><img className="img-logo" src={logo}/></a>
-            </div>
+            <a className="link-logo" href="/"><img className="img-logo" src={logo}/></a>
             <ul className="nav-ul-links">
                 <li className="nav-li-link"><a href="/" className="nav-link-primary">Home</a></li>
                 <li className="nav-li-link"><a href="" className="nav-link-primary">Produtos</a></li>
                 <li className="nav-li-link"><a href="" className="nav-link-primary">Contatos</a></li>
                 <li className="nav-li-link"><a href="" className="nav-link-primary">Nossos Clientes</a></li>
                 <li className="nav-li-link"><a href="/authen"><Button buttonStyle="btn-outline" buttonSize="btn-medium" onClick="">Iniciar Sessão</Button></a></li>
-                <div className="menu-icon nav-li-link">
-                    <img className="menu-drop"src={menu} alt=""></img>
-                </div>
+                
             </ul>
-            
-                    
+            <div className="menu-icon">
+                    <FaBars className="faBars"></FaBars>
+            </div>         
         </nav>
     );
 }
--- src/pages/HomeMain/HomeMain.js
+++ src/pages/HomeMain/HomeMain.js
@@ -4,19 +4,22 @@ import "./HomeMain.css";
 import ilutracao from "../../images/Ilustracao.png"
 import { Button } from '../../components/button/button';
 
-function HomeMain(){ 
-        return (
-            <div className="">
-                <Navbar/>
+
+function HomeMain() {
+    return (
+        <div className="">
+            <Navbar />
             <div className="div-home">
-                <div className="div-elemen">
-                    <h1>Dê Mais Versatilidade Aos Projetos Que Você Ama</h1>
-                    <Button buttonStyle="btn-primary" buttonSize="btn-medium">Saiba mais</Button>
+                <div className="first-contain">
+                    <div className="div-elemen">
+                        <h1>Dê Mais Versatilidade Aos Projetos Que Você Ama</h1>
+                        <Button buttonStyle="btn-primary" buttonSize="btn-medium">Saiba mais</Button>
+                    </div>
+                    <div className="div_figu"><img className="ilustra" src={ilutracao} /></div>
                 </div>
-                <div className="div_figu"><img className="ilustra" src={ilutracao}/></div>
             </div>
-            </div>
-        )
+        </div>
+    )
 };
 export default HomeMain;
 
