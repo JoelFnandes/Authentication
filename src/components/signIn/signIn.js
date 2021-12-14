bedf043443978ae32a6d0a733c6b07ad209dbbd5
Joel Fernandes <joelandre333@gmail.com>
2021-12-14 02:48:56 -0300

    recebendo outras informacoes do back(teste)

--- src/components/signIn/signIn.js
+++ src/components/signIn/signIn.js
@@ -5,6 +5,7 @@ import undraw from '../../images/undraw_authentication.png'
 import envelope from '../../icons/envelope-solid.svg'
 import SignUp from "../signUp/signUp";
 import Axios from "axios";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
 function SignIn() {
     const [isModalVisible, setModalVisible] = useState(false);
@@ -21,26 +22,35 @@ function SignIn() {
     Axios.defaults.withCredentials = true;
 
     const [msgError, setMsgError] = useState();
-    
+    const [msgSucc, setMsgSucc] = useState();
 
     const register = () => {
-        Axios.post("http://localhost:4000/register",{
-        name: nameReg,
-        email: emailReg,
-        password: passwordReg,
-        password2: password2Reg,
-    }).then((response) => {
-        console.log(response);
-        setMsgError(response.data);
-    });
-    };
+        Axios.post("http://localhost:4000/register", {
+            name: nameReg,
+            email: emailReg,
+            password: passwordReg,
+            password2: password2Reg,
+
+        }).then((response) => {
+            if (response.data.errors == null) {
+                setMsgSucc(response.data);
+                
+            }
+            else {
+                setMsgError(response.data);
+            }
+
+        });
+
+    }
+
 
     const login = () => {
-        Axios.post("http://localhost:4000/login",{
-        email: email,
-        password: password,
+        Axios.post("http://localhost:4000/login", {
+            email: email,
+            password: password,
 
-    })
+        })
     };
 
     return (
@@ -50,13 +60,14 @@ function SignIn() {
             </div> */}
             <div className="div-align">
                 <div className="div-align-items">
+                    <ul><li>{msgSucc !== undefined ? msgSucc.successes.map(success => <p>{success.message}</p>): null}</li></ul>
                     <h2>Faça login</h2>
                     <div className="div-input">
                         <span src={envelope}></span>
-                        <input className="input-text" type="text" onChange={(e) => {setEmail(e.target.value);}} placeholder="Email" />
+                        <input className="input-text" type="text" onChange={(e) => { setEmail(e.target.value); }} placeholder="Email" />
                     </div>
                     <div className="div-input">
-                        <input className="input-text" type="password" onChange={(e) => {setPassword(e.target.value);}} placeholder="Senha" />
+                        <input className="input-text" type="password" onChange={(e) => { setPassword(e.target.value); }} placeholder="Senha" />
                     </div>
                     <div className="div-input">
                         <div className="div-sign">
@@ -70,22 +81,23 @@ function SignIn() {
                     <img className="draw" src={undraw} alt="authentication" />
                 </div>
             </div>
-            {isModalVisible ?(
-            <SignUp onClose={() => setModalVisible(false)}>
+            {isModalVisible ? (
+                <SignUp onClose={() => setModalVisible(false)}>
                     <div className="div-align-items-modal">
-                        <ul><li>{msgError!== undefined ? msgError.errors.map(error => <p>{error.message}</p>): null }</li></ul>
+                        <ul><li>{msgError !== undefined ? msgError.errors.map(error => <p>{error.message}</p>) : null}</li>
+                        </ul>
                         <h2>Inscreva-se</h2>
                         <div className="div-input">
-                            <input id="nameReg"className="input-text" onChange={(e) => {setNameReg(e.target.value);}} type="text" placeholder="Nome" required/>
+                            <input id="nameReg" className="input-text" onChange={(e) => { setNameReg(e.target.value); }} type="text" placeholder="Nome" required />
                         </div>
                         <div className="div-input">
-                            <input  id="emailReg" className="input-text" onChange={(e) => {setEmailReg(e.target.value);}} type="email" placeholder="Email" required/>
+                            <input id="emailReg" className="input-text" onChange={(e) => { setEmailReg(e.target.value); }} type="email" placeholder="Email" required />
                         </div>
                         <div className="div-input">
-                            <input id="passwordReg" className="input-text" onChange={(e) => {setPasswordReg(e.target.value);}} type="password" placeholder="Senha" required/>
+                            <input id="passwordReg" className="input-text" onChange={(e) => { setPasswordReg(e.target.value); }} type="password" placeholder="Senha" required />
                         </div>
                         <div className="div-input">
-                            <input id="password2Reg" className="input-text" onChange={(e) => {setPassword2Reg(e.target.value);}} type="password" placeholder="Confirme sua senha" required/>
+                            <input id="password2Reg" className="input-text" onChange={(e) => { setPassword2Reg(e.target.value); }} type="password" placeholder="Confirme sua senha" required />
                         </div>
                         <div className="div-input">
                             <div className="div-sign">
@@ -93,8 +105,8 @@ function SignIn() {
                             </div>
                         </div>
                     </div>
-            </SignUp>
-            ): null}
+                </SignUp>
+            ) : null}
         </div>
     )
 };
